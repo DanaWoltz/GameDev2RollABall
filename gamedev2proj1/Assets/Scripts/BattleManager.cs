b9471ba1ca3c33635c70f6b06682c903d95ff650
Dana Woltz <dw952717@ohio.edu>
2021-02-16 15:20:31 -0500

    TodayIsMonday
    Left play on awake checked, I unchecked it so it doesnt play every time you load SC

---
+++ gamedev2proj1/Assets/Scripts/BattleManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+    public bool battleMode;
+
+    public AudioSource battleMusic;
+
+    private GameObject enemy;
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = GameObject.Find("Enemy");
+        player = GameObject.Find("Player");
+    }
+
+    private IEnumerator StartBattle(float waitTime)
+    {
+        battleMusic.Play();
+        enemy.GetComponent<MoveCube>().battleMode = true;
+        player.GetComponent<SimplePlayerController>().battleMode = true;
+        Debug.Log("Battle Started");
+
+        yield return new WaitForSeconds(waitTime);
+
+        BattlePositions();
+        StartCoroutine("EndBattle", 5);
+    }
+
+    private IEnumerator EndBattle(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        enemy.GetComponent<MoveCube>().battleMode = false;
+        player.GetComponent<SimplePlayerController>().battleMode = false;
+        battleMode = false;
+        Debug.Log("Battle Ended");
+    }
+
+    public void StartBattleMode()
+    {
+        StartCoroutine("StartBattle", 2.5f);
+    }
+
+    void BattlePositions()
+    {
+        enemy.transform.position = new Vector3(-3, enemy.transform.position.y, 0);
+        player.transform.position = new Vector3(3, player.transform.position.y, 0);
+    }
+}
